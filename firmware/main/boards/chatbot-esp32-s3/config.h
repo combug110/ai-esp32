44bfdd86a2ddcea74fb108023ca9165058d26a0b
unknown <676843174@qq.com>
2026-07-26 15:54:13 +0800

    add chatbot board init source code.

---
+++ firmware/main/boards/chatbot-esp32-s3/config.h
@@ -0,0 +1,64 @@
+#ifndef _BOARD_CONFIG_H_
+#define _BOARD_CONFIG_H_
+
+#include <driver/gpio.h>
+
+#define Right_Pitch_Pin GPIO_NUM_NC  
+#define Right_Roll_Pin GPIO_NUM_NC   
+#define Left_Pitch_Pin GPIO_NUM_NC
+#define Left_Roll_Pin GPIO_NUM_NC
+#define Body_Pin GPIO_NUM_11    // body 
+#define Head_Pin GPIO_NUM_10	// head
+
+#define AUDIO_INPUT_SAMPLE_RATE  24000
+#define AUDIO_OUTPUT_SAMPLE_RATE 24000
+
+#define AUDIO_I2S_GPIO_MCLK GPIO_NUM_38
+#define AUDIO_I2S_GPIO_WS   GPIO_NUM_13
+#define AUDIO_I2S_GPIO_BCLK GPIO_NUM_14
+#define AUDIO_I2S_GPIO_DIN  GPIO_NUM_12
+#define AUDIO_I2S_GPIO_DOUT GPIO_NUM_45
+
+#define AUDIO_CODEC_USE_PCA9557
+#define AUDIO_CODEC_PA_PIN       GPIO_NUM_NC
+#define AUDIO_CODEC_I2C_SCL_PIN  GPIO_NUM_2
+#define AUDIO_CODEC_I2C_SDA_PIN  GPIO_NUM_1
+#define AUDIO_CODEC_ES8311_ADDR  ES8311_CODEC_DEFAULT_ADDR
+#define AUDIO_CODEC_ES7210_ADDR  0x82
+
+// GPIO1 拉高用于使能板载音频功放
+#define AUDIO_PA_ENABLE_PIN      GPIO_NUM_47
+
+#define BUILTIN_LED_GPIO        GPIO_NUM_NC
+#define BOOT_BUTTON_GPIO        GPIO_NUM_0
+#define VOLUME_UP_BUTTON_GPIO   GPIO_NUM_NC
+#define VOLUME_DOWN_BUTTON_GPIO GPIO_NUM_NC
+
+#define DISPLAY_SPI_SCK_PIN     GPIO_NUM_41
+#define DISPLAY_SPI_MOSI_PIN    GPIO_NUM_40
+#define DISPLAY_DC_PIN          GPIO_NUM_39
+#define DISPLAY_SPI_CS_PIN      GPIO_NUM_21
+
+/******横屏显示******/
+#define DISPLAY_WIDTH   320
+#define DISPLAY_HEIGHT  240
+#define DISPLAY_MIRROR_X true
+#define DISPLAY_MIRROR_Y true
+#define DISPLAY_SWAP_XY false
+
+/******竖屏显示（可选）******/
+/*
+#define DISPLAY_WIDTH   240
+#define DISPLAY_HEIGHT  320
+#define DISPLAY_MIRROR_X false
+#define DISPLAY_MIRROR_Y false
+#define DISPLAY_SWAP_XY false
+*/
+
+#define DISPLAY_OFFSET_X  0
+#define DISPLAY_OFFSET_Y  0
+
+#define DISPLAY_BACKLIGHT_PIN          GPIO_NUM_42
+#define DISPLAY_BACKLIGHT_OUTPUT_INVERT false
+
+#endif // _BOARD_CONFIG_H_
